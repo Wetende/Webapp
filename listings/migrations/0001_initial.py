b6ca5254c68517291346e1a1d9080982474e9f15
Wetende <100572229+Wetende@users.noreply.github.com>
2022-07-07 12:07:15 +0300

    views

--- listings/migrations/0001_initial.py
+++ listings/migrations/0001_initial.py
@@ -15,6 +15,7 @@ class Migration(migrations.Migration):
             name='Listing',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                
             ],
         ),
     ]
--- listings/models.py
+++ listings/models.py
@@ -1,11 +1,12 @@
 from cgi import print_exception
 from ctypes import addressof
-from email.mime import image
-from turtle import title
+
+
 from django.db import models
 
 # Create your models here.
 class Listing(models.Model):
+    
     title = models.CharField(max_length=150, default='property')
     price = models.IntegerField(default='0')
     num_bedrooms = models.IntegerField(default='0')
@@ -13,5 +14,5 @@ class Listing(models.Model):
     square_footage = models.IntegerField(default='0')
     address = models.CharField(max_length=100, default='city')
 #image 
-def __str__(self):
-    return self.
+#def _str_:(self:title)
+    
