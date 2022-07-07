0b0f3d8c54485d4b767e8129555d597510aa53aa
Wetende <100572229+Wetende@users.noreply.github.com>
2022-07-07 09:51:55 +0300

    modified

--- listings/models.py
+++ listings/models.py
@@ -6,7 +6,6 @@ from django.db import models
 
 # Create your models here.
 class Listing(models.Model):
-    pass
 title = models.CharField(max_length=150)
 price = models.IntegerField()
 num_bedrooms = models.IntegerField()
